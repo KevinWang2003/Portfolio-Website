5e759b5953c1b8936da677450356bed0bd6faa8e
KevinWang2003 <wang0071@hz.nl>
2022-02-13 17:09:48 +0100

    finished week 1

--- database/migrations/2022_02_13_134355_create_grades_table.php
+++ database/migrations/2022_02_13_134355_create_grades_table.php
@@ -17,9 +17,11 @@ class CreateGradesTable extends Migration
             $table->id();
             $table->string('course_name');
             $table->string('test_name');
-            $table->decimal('lowest_passing_grade', 3, 1)->default(5.5)->comment('Lowest grade to pass so average calculations can be applied');
+            $table->decimal('lowest_passing_grade', 3, 1)->default(5.5)->
+            comment('Lowest grade to pass so average calculations can be applied');
             $table->decimal('best_grade', 3, 1)->nullable();
-            $table->date('passed_at')->nullable()->comment('Date that the grade was larger than or equal to the lowest passing grade');
+            $table->date('passed_at')->nullable()->
+            comment('Date that the grade was larger than or equal to the lowest passing grade');
             $table->timestamps();
         });
     }
