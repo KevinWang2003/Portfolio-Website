b18781cd8a77a936b02541f0f2db9a35b372dc65
KevinWang2003 <wang0071@hz.nl>
2022-05-22 20:27:29 +0200

    CSS Overhaul

---
+++ resources/views/common/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/layout.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+          content="Dit is een website die door een HBO-ICT student van het HZ University Of Applied Sciences is gemaakt">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Cache-control" content="no-cache">
+    <script src="https://kit.fontawesome.com/5f690037a3.js" crossorigin="anonymous"></script>
+    @yield('head')
+</head>
+
+<body>
+
+<section>
+    @yield('content')
+</section>
+</body>
+
+</html>
