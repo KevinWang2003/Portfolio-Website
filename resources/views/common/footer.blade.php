1825b147fa14cba1e007657800c849fd5e1970c2
KevinWang2003 <wang0071@hz.nl>
2022-06-02 19:24:23 +0200

    Small change

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <a target="_blank" href="https://www.youtube.com/watch?v=dEI7oX0XxJw"><img class="logo" src="/resources/HZ logo.png" width="75px" height="75px" alt="HZ Logo"></a>
+    <center><a target="_blank" href="https://www.youtube.com/watch?v=dEI7oX0XxJw"><img class="logo" src="/resources/HZ logo.png" width="75px" height="75px" alt="HZ Logo"></a></center>
 </footer>
