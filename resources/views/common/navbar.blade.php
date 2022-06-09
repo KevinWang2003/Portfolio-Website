44570fe58460b2451525956547db49baa1356053
KevinWang2003 <wang0071@hz.nl>
2022-06-09 11:16:18 +0200

    test

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -7,15 +7,12 @@
             <li class="{{ Request::path() === 'portfolio' ? 'active' : '' }}"><a href="/portfolio">Portfolio</a></li>
             <li class="{{ Request::path() === 'dashboard' ? 'active' : '' }}"><a href="/dashboard">Dashboard</a></li>
             <li class="{{ Request::path() === 'index' ? 'active' : '' }}"><a href="{{ route('articles.index') }}">Blog</a></li>
-            <li class="{{ Request::path() === 'faqs' ? 'active' : '' }}"><a href="{{ route('faqs.index') }}">FAQ</a>
-            </li>
-                @if(Auth::check())
-                    <a href="{{ route('logout') }}" class="logout"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Uitloggen</a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-
+            <li class="{{ Request::path() === 'faqs' ? 'active' : '' }}"><a href="{{ route('faqs.index') }}">FAQ</a></li>
+            @if(Auth::check())
+                <a href="{{ route('logout') }}" class="logout" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Uitloggen</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
                 @elseif(!Auth::check())
                     <li><a class="{{ Request::path() === 'login' ? 'active' : '' }}" href="/login">Login</a></li>
                 @endif
