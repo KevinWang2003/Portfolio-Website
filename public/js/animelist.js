4df2e9c9192cc9f2650b38dd3f92d1c66f3b9f5c
KevinWang2003 <wang0071@hz.nl>
2022-06-14 09:53:09 +0200

    Small updates

--- public/js/animelist.js
+++ public/js/animelist.js
@@ -38,8 +38,7 @@ async function updatePage(list, indicator, amount) {
         progress.innerHTML = `<p class="text-center" style="color: black">${indicator + 1} / ${amount}</p>`
         const ul = document.createElement("div");
         ul.innerHTML =
-            `
-                <div class="container">
+            `<div class="container">
                 <div class="align-items-center">
                     <div class="card mb-3">
                         <div class="row no-gutters">
@@ -57,8 +56,7 @@ async function updatePage(list, indicator, amount) {
                         </div>
                     </div>
                 </div>
-            </div>
-`;
+            </div>`;
         domElement.append(ul);
     })
 }
@@ -78,7 +76,7 @@ async function getCount(){
         return count;
     } catch (err) {
     console.error("Error: ", err);
-}
+    }
 }
 
 
@@ -123,60 +121,3 @@ async function fetchAnimeData(relation) {
 
 init()
 
-// Here we define our query as a multi-line string
-// Storing it in a separate .graphql/.gql file is also possible
-// var query = `
-// query ($id: Int) { # Define which variables will be used in the query (id)
-//   Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-//     id
-//     title {
-//       romaji
-//       english
-//       native
-//     }
-//   }
-//   User (name: user) {
-//
-//   }
-// }
-// `;
-//
-// // Define our query variables and values that will be used in the query request
-// var variables = {
-//     id: 15125,
-//     user: xTheNightShade
-// };
-//
-// // Define the config we'll need for our Api request
-// var url = 'https://graphql.anilist.co',
-//     options = {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Accept': 'application/json',
-//         },
-//         body: JSON.stringify({
-//             query: query,
-//             variables: variables
-//         })
-//     };
-//
-// // Make the HTTP Api request
-// fetch(url, options).then(handleResponse)
-//     .then(handleData)
-//     .catch(handleError);
-//
-// function handleResponse(response) {
-//     return response.json().then(function (json) {
-//         return response.ok ? json : Promise.reject(json);
-//     });
-// }
-//
-// function handleData(data) {
-//     console.log(data);
-// }
-//
-// function handleError(error) {
-//     alert('Error, check console');
-//     console.error(error);
-// }
